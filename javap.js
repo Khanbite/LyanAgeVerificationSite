14648908edda6e2dbe9e41b74e9a415ec12bdee2
Khani Lyan <klyan@my.waketech.edu>
2025-01-29 13:33:26 -0500

    slay

--- javap.js
+++ javap.js
@@ -5,11 +5,14 @@ let age;
 subButton.onclick = function(){
     let age = myText.value;
     age = Number(age);
-    if(age <=0){
+    if(age <0){
         myResult.textContent= "You cant be that young!";
     }
     else if(age <=16){
         myResult.textContent= "You are way too young!";
+    }
+    else if(age > 16 && age < 18){
+        myResult.textContent= "You're close...";
     }
     else if(age>=100){
         myResult.textContent= "You are way too old!";
