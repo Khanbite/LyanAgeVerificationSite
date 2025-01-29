7680e3a0896f2eceef8bbd77e4819c113b7317fa
Khani Lyan <klyan@my.waketech.edu>
2025-01-29 13:27:42 -0500

    age check site created

---
+++ javap.js
@@ -0,0 +1,23 @@
+const myText = document.getElementById("myText");
+const subButton = document.getElementById("subButton");
+const myResult = document.getElementById("myResult");
+let age;
+subButton.onclick = function(){
+    let age = myText.value;
+    age = Number(age);
+    if(age <=0){
+        myResult.textContent= "You cant be that young!";
+    }
+    else if(age <=16){
+        myResult.textContent= "You are way too young!";
+    }
+    else if(age>=100){
+        myResult.textContent= "You are way too old!";
+    }
+    else if(age===18){
+        myResult.textContent= "You are the perfect age!;)";
+    }
+    else{
+       
+    }
+};
